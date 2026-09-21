d65ae0392f681102e61626dc18d2f636e554c285
olganuzhnaya <olganuzhnayairkutsk@gmail.com>
2026-09-21 12:12:05 +0800

    Improve universal crypto API

--- js/crypto-api.js
+++ js/crypto-api.js
@@ -1,55 +1,33 @@
 async function getCryptoData(coinId) {
 
-
     const url =
         `https://api.coingecko.com/api/v3/coins/${coinId}`;
 
-
     try {
 
-
-        const response =
-            await fetch(url);
-
-
+        const response = await fetch(url);
 
         if (!response.ok) {
-
 
             throw new Error(
                 `HTTP error: ${response.status}`
             );
 
-
         }
 
-
-
-        const data =
-            await response.json();
-
-
+        const data = await response.json();
 
         return data;
 
-
-
     }
 
-
-    catch(error) {
-
+    catch (error) {
 
         console.error(
             "Crypto API error:",
             error
         );
 
-
         return null;
-
-
     }
-
-
 }
